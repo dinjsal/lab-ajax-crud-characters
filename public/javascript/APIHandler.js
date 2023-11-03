41df4a8cc9dd410509339631505331cc4df3845f
Dinah Salazar <dinahsalazar@gmail.com>
2023-11-03 18:32:02 +0900

    Correct lab

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -6,18 +6,52 @@ class APIHandler {
   }
 
   async getFullList() {
-    const result = await this.api.get("/characters");
+    try {
+      const result = await this.api.get("/characters");
     return result;
+  }
+    catch {
+      (err) => console.log(err);
+    }  
   }
 
   async getOneRegister(id) {
-    const result = await this.api.get("/characters/" + id);
+    try {
+      const result = await this.api.get("/characters/" + id);
     return result;
   }
+    catch {
+      (err) => console.log(err)
+    };
+  }
 
-  createOneRegister() {}
+  async createOneRegister(character) {
+    try {
+      const result = await this.api.post('/characters/', character);
+      return result;
+      }
+      catch {
+        (err) => console.log(err);
+      }
+    }
 
-  updateOneRegister() {}
+  async updateOneRegister(character) {
+    try {
+      const result = await this.api.put('/characters/' + character.id, character);
+      return result;
+      }
+      catch {
+        (err) => console.log(err);
+      }
+  }
 
-  deleteOneRegister() {}
+  async deleteOneRegister(id) {
+    try {
+      const result = await this.api.delete('/characters/' + id);
+      return result;
+    }
+    catch {
+      (err) => console.log(err);
+    }
+  }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -27,21 +27,64 @@ window.addEventListener("load", () => {
       const charactersDiv = document.querySelector(".characters-container");
       charactersDiv.innerHTML += `
       <div class="character-info">
-      <div class="name"> Character Name: ${character.name}</div>
-      <div class="occupation"> Character Occupation: ${character.occupation}</div>
-      <div class="weapon"> Character Weapon: ${character.weapon}</div>
+      <div class="name"> Character Name: ${result.data.name}</div>
+      <div class="occupation"> Character Occupation: ${result.data.occupation}</div>
+      <div class="weapon"> Character Weapon: ${result.data.weapon}</div>
         `;
     });
 
   document
     .getElementById("delete-one")
-    .addEventListener("click", function (event) {});
+    .addEventListener("click", async (event) => {
+      const id = document.querySelector(
+        "input[name=character-id-delete]"
+      ).value;
+      const result = await charactersAPI.deleteOneRegister(id);
+    });
 
   document
     .getElementById("edit-character-form")
-    .addEventListener("submit", function (event) {});
+    .addEventListener("submit", async (event) => {
+      event.preventDefault();
+      const id = document.querySelector(
+        "#edit-character-form input[name=chr-id]"
+      ).value;
+      const name = document.querySelector(
+        "#edit-character-form input[name=name]"
+      ).value;
+      const occupation = document.querySelector(
+        "#edit-character-form input[name=occupation]"
+      ).value;
+      const weapon = document.querySelector(
+        "#edit-character-form input[name=weapon]"
+      ).value;
+      const cartoon = document.querySelector(
+        "#edit-character-form input[name=cartoon]"
+      ).checked;
+      const character = {
+        id,
+        name,
+        occupation,
+        weapon,
+        cartoon,
+      };
+      const result = await charactersAPI.updateOneRegister(character);
+    });
 
   document
     .getElementById("new-character-form")
-    .addEventListener("submit", function (event) {});
+    .addEventListener("submit", async (event) => {
+      event.preventDefault();
+      const name = document.querySelector("input[name=name]").value;
+      const occupation = document.querySelector("input[name=occupation]").value;
+      const weapon = document.querySelector("input[name=weapon]").value;
+      const cartoon = document.querySelector("input[name=cartoon]").checked;
+      const character = {
+        name,
+        occupation,
+        weapon,
+        cartoon,
+      };
+      const result = await charactersAPI.createOneRegister(character);
+    });
 });
