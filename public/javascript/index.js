de37fbb1a023c23d149f6a5ba62f147758b611ff
Dinah Salazar <dinahsalazar@gmail.com>
2023-11-03 15:51:12 +0900

    Update js files

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,36 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+// const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler("https://ih-crud-api.herokuapp.com");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+window.addEventListener("load", () => {
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", async (event) => {
+      const result = await charactersAPI.getFullList();
+      const charactersDiv = document.querySelector(".characters-container");
+      charactersDiv.innerHTML = "";
+      result.data.forEach((character) => {
+        charactersDiv.innerHTML += `
+      <div class="character-info">
+      <div class="name"> Character Name: ${character.name}</div>
+      <div class="occupation"> Character Occupation: ${character.occupation}</div>
+      <div class="weapon"> Character Weapon: ${character.weapon}</div>
+        `;
+      });
+    });
 
-  });
+  document
+    .getElementById("fetch-one")
+    .addEventListener("click", function (event) {});
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document
+    .getElementById("delete-one")
+    .addEventListener("click", function (event) {});
 
-  });
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", function (event) {});
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
+  document
+    .getElementById("new-character-form")
+    .addEventListener("submit", function (event) {});
 });
